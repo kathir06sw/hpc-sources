3bf29cc28811820c138418182cf56563de76c713
Manickam Subramaniam <manickam.manic91@gmail.com>
2020-10-15 17:02:00 +0530

    Update emp_filter.py
    update path

--- emp_filter.py
+++ emp_filter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-df = pd.read_csv('/home/admin/HPC/slurm-demo/emp-data.csv')
+df = pd.read_csv('/var/tmp/steerwise/emp-data.csv')
 new_employees_filter = df["Year of Joining"] > 2010;
 new_employees = df[new_employees_filter]
-new_employees.to_csv("/home/admin/HPC/slurm-demo/new_employees.csv", header='column_names')
+new_employees.to_csv("/var/tmp/steerwise/new_employees.csv", header='column_names')
