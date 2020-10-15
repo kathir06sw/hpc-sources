5324abc7c4f1db8f79f018545e1e9c73be429270
Manickam Subramaniam <manickam.manic91@gmail.com>
2020-10-15 21:36:11 +0530

    Update emp_filter.cpp

--- emp_filter.cpp
+++ emp_filter.cpp
@@ -3,28 +3,26 @@
 using namespace std;
 int main()
 {
-	fstream new_file; 
-	ofstream out_file("/var/tmp/steerwise/tx_employees.csv");
-	new_file.open("/var/tmp/steerwise/emp-data.csv",ios::in);   
-	if(!new_file) {
+	ofstream out_file("/mnt/d/DevOps/hpc-us-west-2/tx_employees.csv");
+	std::ifstream infile("/mnt/d/DevOps/hpc-us-west-2/emp-data-small.csv");
+
+	if(!infile) {
 		cout<<"No such file"; 
 	}
 	else { 
-		string ch; 
-		if(out_file.is_open()){
-			while (!new_file.eof()) { 
-				new_file >>ch;
-				int found = ch.find("TX"); 
-				if(found>0 ){
-					//cout << ch;   
-					out_file << ch;
-					out_file << "\n";
-				}
+		for( std::string line; getline( infile, line ); )
+		{
+			// cout <<"line"<<line;
+			int found = line.find("TX"); 
+			if(found>0 ){
+				// cout << line;   
+				out_file << line;
+				out_file << "\n";
+			}
 		}
-		new_file.close();  
+		infile.close();  
 		out_file.close();
 		return 0;
-		}
 	
 	}
 }
