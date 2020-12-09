29fea237856e0c56933a561793e9f262cc313213
kathir06sw <74909823+kathir06sw@users.noreply.github.com>
2020-12-09 14:53:12 +0530

    Update emp_filter.py

--- emp_filter.py
+++ emp_filter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-df = pd.read_csv('/var/tmp/steerwise/emp-data.csv')
+df = pd.read_csv('/home/ubuntu/emp-data.csv')
 new_employees_filter = df["Year of Joining"] > 2010;
 new_employees = df[new_employees_filter]
-new_employees.to_csv("/var/tmp/steerwise/new_employees.csv", header='column_names',index=False)
+new_employees.to_csv("/home/ubuntu/new_employees.csv", header='column_names',index=False)
